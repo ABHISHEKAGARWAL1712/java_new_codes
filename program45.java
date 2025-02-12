0b6dd2267f123d818808e9f93386a2667d8fa506
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2025-02-12 21:26:57 +0530

    calculating the ceil and floor of the number usinf build in function

---
+++ program45.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class program45 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to found the ceil and floor value");
+        double n = sc.nextDouble();
+        System.out.println("Ceil value of "+n+" is "+Math.ceil(n));
+        System.out.println("Floor value of "+n+" is "+Math.floor(n));
+    }
+    
+}
