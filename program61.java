d963f0d41fe31704c4f02a0f5223875b1f9c28b9
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2025-02-16 00:27:52 +0530

    Reverse the array by writing the for loop from back method1

---
+++ program61.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class program61 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int [] arr = {10,20,30,40};
+        int n = arr.length;
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
