4567e5d48389b69e80fad5bf51e45035eb2ac578
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2025-02-15 13:53:01 +0530

    initializing the value to the array directly when value is given

---
+++ program53.java
@@ -0,0 +1,7 @@
+public class program53 {
+    public static void main(String args[]){
+        int [] arr = {10,20,30,40,50};        //Drirectly initializing array when value in given
+        System.out.println(arr[3]);
+    }
+    
+}
