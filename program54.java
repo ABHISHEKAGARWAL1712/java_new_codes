f306be1ba9347010328975216b80ec81a4a57aad
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2025-02-15 13:57:10 +0530

    Calculating the length of array using .length function

---
+++ program54.java
@@ -0,0 +1,7 @@
+public class program54 {
+    public static void main(String args[]){
+        int [] arr = {10, 20, 30, 40 , 50};
+        System.out.println(arr.length);   // to get the length of the array we use .length property
+    }
+    
+}
