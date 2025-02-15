44b06fa7b72c2ec2f79a1a11d6e90f25b2d2913d
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2025-02-15 22:49:26 +0530

    Read an array of N length and print the sum of the input array

---
+++ program57.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class program57 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            System.out.println("Enter the element of the array");
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            int value = arr[i];
+            sum = sum + value;
+        }
+        System.out.println("The sum of the input array is:" + sum);
+    }
+    
+}
